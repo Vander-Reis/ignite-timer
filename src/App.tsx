3067e048ff5079ec30b7a57686fc1e0674212504
Vander-Reis <vanderreis2017@outlook.com>
2022-09-14 19:29:26 -0300

    create styles page home

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import { ThemeProvider } from 'styled-components'
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom'
 
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
-import { Router } from './Router';
+import { Router } from './Router'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <BrowserRouter>
-        <Router/>
+        <Router />
       </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
