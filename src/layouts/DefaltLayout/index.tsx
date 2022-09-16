3174a0d1d8f7cb702ccdaa83034c5eefe4d99a23
Vander-Reis <vanderreis2017@outlook.com>
2022-09-16 18:38:19 -0300

    create page history

--- src/layouts/DefaltLayout/index.tsx
+++ src/layouts/DefaltLayout/index.tsx
@@ -1,11 +1,11 @@
-import { Header } from "../../Components/Header";
-import { Outlet } from "react-router-dom";
-import { LayoutContainer } from "./styles";
+import { Header } from '../../Components/Header'
+import { Outlet } from 'react-router-dom'
+import { LayoutContainer } from './styles'
 export function DefaultLayout() {
   return (
     <LayoutContainer>
       <Header />
       <Outlet />
     </LayoutContainer>
-  );
+  )
 }
